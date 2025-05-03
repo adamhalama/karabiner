76f117b5d4a7e9c829cbab366ec7ce91644b1de6
Jan Adam Halama <adamhalama@outlook.com>
2025-05-03 11:16:11 +0200

    add Preview to u, add qbittorrent under d as download

--- rules/sublayers/utils.ts
+++ rules/sublayers/utils.ts
@@ -3,5 +3,7 @@ import { app } from "../../utils.ts";
 export const utils = {
   u: {
     t: app("qBittorrent"), 
+    d: app("qBittorrent"), // download
+    p: app("Preview"), // preview, PDF
   },
 } as const;
