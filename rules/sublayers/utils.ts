caa9ee5da94925aec5304271076183917fe93e8d
Jan Adam Halama <adamhalama@outlook.com>
2025-05-08 13:56:21 +0200

    add IINA to u sublayer as i and v

--- rules/sublayers/utils.ts
+++ rules/sublayers/utils.ts
@@ -5,7 +5,11 @@ export const utils = {
     t: app("qBittorrent"), 
     d: app("qBittorrent"), // download
     p: app("Preview"), // preview, PDF
+
     r: app("Windows App"), // remote desktop
     w: app("Windows App"), // windows
+
+    i: app("IINA"), // IINA
+    v: app("IINA"), // video
   },
 } as const;
