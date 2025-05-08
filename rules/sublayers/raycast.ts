920fb31f971b6ea3f147c3d84c9016730d6d523a
Jan Adam Halama <adamhalama@outlook.com>
2025-05-08 17:35:25 +0200

    add focus as F and edit focus as E under R layer

--- rules/sublayers/raycast.ts
+++ rules/sublayers/raycast.ts
@@ -4,9 +4,11 @@ export const raycast = {
   r: {
     p: open("raycast://extensions/jomifepe/bitwarden/search"), // passwords
     c: open("raycast://extensions/raycast/raycast/confetti"),
-    e: open("raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"),
     a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
     h: open("raycast://extensions/raycast/clipboard-history/clipboard-history"),
     s: open("raycast://extensions/raycast/screenshots/search-screenshots"),
+
+    f: open("raycast://extensions/raycast/raycast-focus/start-focus-session"),
+    e: open("raycast://extensions/raycast/raycast-focus/search-focus-categories"), //edit focus
   },
 } as const;
