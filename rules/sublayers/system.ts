c47491f01b128f8e27b5b46fd70ffce6e2ca9a69
Jan Adam Halama <adamhalama@outlook.com>
2025-07-08 23:42:50 +0200

    add keyboard brightness control with system syblayer

--- rules/sublayers/system.ts
+++ rules/sublayers/system.ts
@@ -6,7 +6,13 @@ export const system = {
     j: { to: [{ key_code: "volume_decrement" }] },
     i: { to: [{ key_code: "display_brightness_increment" }] },
     k: { to: [{ key_code: "display_brightness_decrement" }] },
-    l: { to: [{ key_code: "q", modifiers: ["right_control", "right_command"] }] },
+
+    // ↑ keyboard back-light brighter
+    o: { to: [{ apple_vendor_top_case_key_code: "illumination_up", repeat: true }] },
+    // ↓ keyboard back-light dimmer
+    l: { to: [{ apple_vendor_top_case_key_code: "illumination_down", repeat: true }] },
+  
+
     p: { to: [{ key_code: "play_or_pause" }] },
     semicolon: { to: [{ key_code: "fastforward" }] },
     e: open("raycast://extensions/thomas/elgato-key-light/toggle?launchType=background"),
