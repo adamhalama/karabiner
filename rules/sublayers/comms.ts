db6be5311d9bae0f4378b8da1b68ea8e0db5ef78
Jan Adam Halama <adamhalama@outlook.com>
2025-09-01 10:04:51 +0200

    add imessages and slack to comms layer

--- rules/sublayers/comms.ts
+++ rules/sublayers/comms.ts
@@ -4,8 +4,10 @@ export const comms = {
   c: {
     m: app("Messenger"),
     w: app("Texts"),          // WhatsApp → Texts
-    i: app("Texts"),          // Messages  → Texts
+    // i: app("Texts"),          // Messages  → Texts
     d: app("legcord"),        // Discord
     t: app("Microsoft Teams"),
+    s: app("Slack"),
+    i: app("Messages"),
   },
 } as const;
